160bbf9cd60e5c4056e773846de434c565d4b1f3
Yuri Reis <yuri.reis@msn.com>
2019-08-31 13:48:20 -0300

    Improvement: removing inline style

--- js/location-card.js
+++ js/location-card.js
@@ -10,36 +10,75 @@ class LocationCard extends HTMLElement {
     set location(location) {
         // settled this.root because I'm using shadow DOM
         this.root.innerHTML = `
-            <div style="width: 246px; padding: 0 8px; margin-bottom: 25px;">
-                <div style="object-fit: cover">
-                    <img style="width: 100%; height: 164px; object-fit: cover;" src="${location.photo}" alt="" srcset="">
-                </div>
-                <div style="width: 100%; margin: 0px !important;
-                word-wrap: break-word !important;
-                font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
-                font-size: 12px !important;
-                font-weight: 800 !important;
-                line-height: 1.3333333333333333em !important;
-                color: #484848 !important;">
-                    <span style="text-transform: uppercase; color: #767676">${location.property_type}</span>
-                </div>
-                <div style="width: 100%; margin: 0px !important;
-                word-wrap: break-word !important;
-                font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
-                font-size: 18px !important;
-                font-weight: 600 !important;
-                line-height: 1.4444444444444444em !important;
-                color: #484848 !important;">
-                    <div style="display: inline-block !important; line-height: 22px !important;">${location.name}</div>
-                </div>
-                <div>
-                    <span style="margin: 0px !important;
+            <style>
+                .card-container {
+                    width: 246px;
+                    padding: 0 8px;
+                    margin-bottom: 25px;
+                }
+
+                .card-image-container {
+                    object-fit: cover;
+                }
+
+                .card-image-container > img {
+                    width: 100%;
+                    height: 164px;
+                    object-fit: cover;
+                }
+
+                .location-property-type {
+                    width: 100%; margin: 0px !important;
+                    word-wrap: break-word !important;
+                    font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+                    font-size: 12px !important;
+                    font-weight: 800 !important;
+                    line-height: 1.3333333333333333em !important;
+                    color: #484848 !important;
+                }
+
+                .location-property-type > span {
+                    text-transform: uppercase;
+                    color: #767676;
+                }
+
+                .location-name {
+                    width: 100%; margin: 0px !important;
+                    word-wrap: break-word !important;
+                    font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+                    font-size: 18px !important;
+                    font-weight: 600 !important;
+                    line-height: 1.4444444444444444em !important;
+                    color: #484848 !important;
+                }
+
+                .location-name > div {
+                    display: inline-block !important;
+                    line-height: 22px !important;
+                }
+
+                .location-price {
+                    margin: 0px !important;
                     word-wrap: break-word !important;
                     font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
                     font-size: 14px !important;
                     font-weight: 400 !important;
                     line-height: 1.2857142857142858em !important;
-                    color: #484848 !important;">R$${location.price}/Noite</span>
+                    color: #484848 !important;
+                }
+            </style>
+            <div class="card-container">
+                <div class="card-image-container">
+                    <img src="${location.photo}" alt="" srcset="">
+                </div>
+                <div class="location-property-type">
+                    <span>${location.property_type}</span>
+                </div>
+                <div class="location-name">
+                    <div>${location.name}</div>
+                </div>
+                <div>
+                    <span class="location-price">R$${location.price}/Noite</span>
                 </div>
             </div>
         `
